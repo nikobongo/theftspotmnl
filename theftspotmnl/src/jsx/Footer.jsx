0bd7cb580b9266602537636907d2796511ad3313
niko <nikobongo.ra@gmail.com>
2025-07-21 19:04:06 +0800

    lastonenatalaga

--- theftspotmnl/src/jsx/Footer.jsx
+++ theftspotmnl/src/jsx/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>© 2025 Theft News Classifier — The Bad Habits</p>
+                <p>© 2025 TheftSpotMNL — The Bad Habits</p>
                 <div className="social-icons">
                     <a href="https://www.facebook.com/creepbone" target="_blank" rel="noopener noreferrer"><img src="/img/facebook-icon.png" alt="Facebook" /></a>
                     <a href="https://www.instagram.com/pjblcrz_/" target="_blank" rel="noopener noreferrer"><img src="/img/twitter-icon.png" alt="Twitter" /></a>
